f63a10147356f790a3007c56e22227451827141f
David Vega <dvega920@gmail.com>
2019-11-07 11:26:29 -0500

    refactored ajax calls

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
 
-
-
-
-
     var apiKey = "5dbf122089ddb1d926963e42677ead3e34c4cb2860623";
 
     // Start Click for JQuery
@@ -17,16 +13,20 @@ $(document).ready(function () {
 
                 var h3 = $("<h3>");
                 var p = $("<p>");
+                var img = $("<img>");
+                var a = $("<a>");
+                var div = $("<div>");
                 var test = $(".testFront");
                 test.empty();
                 test.append(h3);
-                h3.append("Title: " + result.title);
-                test.append("<p>" + "Description: " + result.description + "</p>");
-                test.append("<p>" + "Why we recommend it: " + "</p>" + "<p>" + "W3Schools is a fantanstic resource for learning to code from the ground up. We particularly like the Try It Yourself feature that allows you to make live code edits.");
-                var $imgTag = $("<img>").attr("src", result.image);
-                test.append($imgTag);
-                var $aTag = $("<a>").attr("href", result.url).text(result.url);
-                test.append($aTag);
+                h3.append(result.title);
+                h3.append(p.text(result.description));
+                test.append(`<p> Why we recommend it: </p> <p> JQuery is a Javascript library that implements the same functionality with abbreviated, easily readable code.</p>`);
+                test.append(a.attr("href", result.url).text(result.url));
+                a.attr("target", "_blank");
+                test.append(div).addClass("img");
+                $(".img").append(img.attr("src", "https://miro.medium.com/max/1200/0*s1Goua9K1MuNuapv.jpg"));
+
             }
         });
     });
@@ -46,13 +46,15 @@ $(document).ready(function () {
                 var test = $(".testFront");
                 test.empty();
                 test.append(h3);
-                h3.append("Title: " + result.title);
-                test.append("<p>" + "Description: " + result.description + "</p>");
-                test.append("<p>" + "Why we recommend it: " + "</p>" + "<p>" + "W3Schools is a fantanstic resource for learning to code from the ground up. We particularly like the Try It Yourself feature that allows you to make live code edits.");
-                var $imgTag = $("<img>").attr("src", result.image);
-                test.append($imgTag);
+                h3.append(result.title);
+                test.append(p.text(result.description));
+                test.append(`<p> Why we recommend it: </p> <p> Moment JS is especially helpful in provided multiple formats for time and date. </p>`);
                 var $aTag = $("<a>").attr("href", result.url).text(result.url);
                 test.append($aTag);
+                $("a").attr("target", "_blank");
+                var $imgTag = $("<img>").attr("src", "https://miro.medium.com/max/1110/1*IBE92hS18PR9WEhMB3xz1w.jpeg");
+                test.append($imgTag);
+
             }
         });
     });
@@ -72,13 +74,15 @@ $(document).ready(function () {
                 var test = $(".testFront");
                 test.empty();
                 test.append(h3);
-                h3.append("Title: " + result.title);
-                test.append("<p>" + "Description: " + result.description + "</p>");
-                test.append("<p>" + "Why we recommend it: " + "</p>" + "<p>" + "W3Schools is a fantanstic resource for learning to code from the ground up. We particularly like the Try It Yourself feature that allows you to make live code edits.");
-                var $imgTag = $("<img>").attr("src", result.image);
-                test.append($imgTag);
+                h3.append(result.title);
+                test.append(p.text(result.description));
+                test.append(`<p> Why we recommend it: </p> <p> Bootstrap offers comprehensive CSS layouts for HTML documents. This is easy to implement using their CDN and documentation.</p>`);
                 var $aTag = $("<a>").attr("href", result.url).text(result.url);
                 test.append($aTag);
+                $("a").attr("target", "_blank");
+                var $imgTag = $("<img>").attr("src", result.image);
+                test.append($imgTag);
+
             }
         });
     });
@@ -98,13 +102,15 @@ $(document).ready(function () {
                 var test = $(".testFront");
                 test.empty();
                 test.append(h3);
-                h3.append("Title: " + result.title);
-                test.append("<p>" + "Description: " + result.description + "</p>");
-                test.append("<p>" + "Why we recommend it: " + "</p>" + "<p>" + "W3Schools is a fantanstic resource for learning to code from the ground up. We particularly like the Try It Yourself feature that allows you to make live code edits.");
-                var $imgTag = $("<img>").attr("src", result.image);
-                test.append($imgTag);
+                h3.append("GitHub: " + result.title);
+                test.append(p.text(result.description));
+                test.append(`<p> Why we recommend it: </p> <p> GitHub is a fantastic website for version control of your project. They also offer an IO link for live project demonstration.</p>`);
                 var $aTag = $("<a>").attr("href", result.url).text(result.url);
                 test.append($aTag);
+                $("a").attr("target", "_blank");
+                var $imgTag = $("<img>").attr("src", result.image);
+                test.append($imgTag);
+
             }
         });
     });
@@ -119,18 +125,22 @@ $(document).ready(function () {
             success: function (result) {
                 console.log(result);
 
-                var h3 = $("<h3>");
                 var p = $("<p>");
+                var h3 = $("<h3>");
+                var div = $("<div>");
                 var test = $(".testFront");
                 test.empty();
                 test.append(h3);
-                h3.append("Title: " + result.title);
-                test.append("<p>" + "Description: " + result.description + "</p>");
-                test.append("<p>" + "Why we recommend it: " + "</p>" + "<p>" + "W3Schools is a fantanstic resource for learning to code from the ground up. We particularly like the Try It Yourself feature that allows you to make live code edits.");
-                var $imgTag = $("<img>").attr("src", result.image);
-                test.append($imgTag);
+                h3.append(result.title);
+                test.append(p.text(result.description));
+                test.append(`<p> Why we recommend it: </p> <p> Mozilla Developer Network offers comprehensive documentation on webdevelopment languages, including HTML, CSS and Javascript.</p>`);
                 var $aTag = $("<a>").attr("href", result.url).text(result.url);
                 test.append($aTag);
+                $("a").attr("target", "_blank");
+                test.append(div).addClass("img");
+                var $imgTag = $("<img>").attr("src", result.image);
+                test.append($imgTag);
+
             }
         });
     });
@@ -145,18 +155,22 @@ $(document).ready(function () {
             success: function (result) {
                 console.log(result);
 
-                var h3 = $("<h3>");
                 var p = $("<p>");
+                var h3 = $("<h3>");
+                var div = $("<div>");
                 var test = $(".testFront");
                 test.empty();
                 test.append(h3);
-                h3.append("Title: " + result.title);
-                test.append("<p>" + "Description: " + result.description + "</p>");
-                test.append("<p>" + "Why we recommend it: " + "</p>" + "<p>" + "W3Schools is a fantanstic resource for learning to code from the ground up. We particularly like the Try It Yourself feature that allows you to make live code edits.");
-                var $imgTag = $("<img>").attr("src", result.image);
-                test.append($imgTag);
+                h3.append(result.title);
+                test.append(p.text(result.description));
+                test.append(`<p> Why we recommend it: </p> <p> Adobe offers beautiful stock photos and photo editing capabilities.</p>`);
                 var $aTag = $("<a>").attr("href", result.url).text(result.url);
                 test.append($aTag);
+                $("a").attr("target", "_blank");
+                test.append(div).addClass("img");
+                var $imgTag = $("<img>").attr("src", result.image);
+                test.append($imgTag);
+
             }
         });
     });
@@ -171,18 +185,22 @@ $(document).ready(function () {
             success: function (result) {
                 console.log(result);
 
-                var h3 = $("<h3>");
                 var p = $("<p>");
+                var h3 = $("<h3>");
+                var div = $("<div>");
                 var test = $(".testFront");
                 test.empty();
                 test.append(h3);
-                h3.append("Title: " + result.title);
-                test.append("<p>" + "Description: " + result.description + "</p>");
-                test.append("<p>" + "Why we recommend it: " + "</p>" + "<p>" + "W3Schools is a fantanstic resource for learning to code from the ground up. We particularly like the Try It Yourself feature that allows you to make live code edits.");
-                var $imgTag = $("<img>").attr("src", result.image);
-                test.append($imgTag);
+                h3.append(result.title);
+                test.append(p.text(result.description));
+                test.append(`<p> Why we recommend it: </p> <p> Code not working? Take your question to Stack Overflow, a helpful forum where other developers can review your code.</p>`);
                 var $aTag = $("<a>").attr("href", result.url).text(result.url);
                 test.append($aTag);
+                $("a").attr("target", "_blank");
+                test.append(div).addClass("img");
+                var $imgTag = $("<img>").attr("src", result.image);
+                test.append($imgTag);
+
             }
         });
     });
@@ -200,16 +218,20 @@ $(document).ready(function () {
 
                 var p = $("<p>");
                 var h3 = $("<h3>");
+                var div = $("<div>");
                 var test = $(".testFront");
                 test.empty();
                 test.append(h3);
-                h3.append("Title: " + result.title);
-                test.append("<p>" + "Description: " + result.description + "</p>");
-                test.append("<p>" + "Why we recommend it: " + "</p>" + "<p>" + "W3Schools is a fantanstic resource for learning to code from the ground up. We particularly like the Try It Yourself feature that allows you to make live code edits.");
-                var $imgTag = $("<img>").attr("src", result.image);
-                p.append($imgTag);
+                h3.append(result.title);
+                test.append(p.text(result.description));
+                test.append(`<p> Why we recommend it: </p> <p> W3Schools is a fantanstic resource for learning to code from the ground up. We particularly like the Try It Yourself feature that allows you to make live code edits.</p>`);
                 var $aTag = $("<a>").attr("href", result.url).text(result.url);
                 test.append($aTag);
+                $("a").attr("target", "_blank");
+                test.append(div).addClass("img");
+                var $imgTag = $("<img>").attr("src", "https://upload.wikimedia.org/wikipedia/commons/3/3e/W3Schools_logo.png");
+                test.append($imgTag);
+
             }
         })
     })
